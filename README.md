3d4f784a4b1cec3aa0b5b7db7b2b6c71841f5a76
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2021-01-15 11:26:24 +0100

    Add example with fake vs true multiple asynchronous invocation

---
+++ README.md
@@ -0,0 +1,9 @@
+# True async vs fake async
+
+## True async example result
+
+![true-results](assets/true-async-result.png)
+
+## Fake async example result
+
+![fake-results](assets/fake-async-result.png)
---
+++ TestingAsyncSync/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestingAsyncSync
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Starting test...");
+            // await TrueMultipleAsync();
+            await FakeMultipleAsync();
+            Console.WriteLine("Finished test...");
+            Console.WriteLine($"Test duration: {sw.ElapsedMilliseconds} ms");
+        }
+
+        public static async Task FakeMultipleAsync()
+        {
+            var fakeTasks = Enumerable.Range(1, 10).Select(SingleFakeAsyncOperation);
+            await Task.WhenAll(fakeTasks);
+        }
+
+        public static async Task TrueMultipleAsync()
+        {
+            var trueTasks = Enumerable.Range(1, 10).Select(SingleTrueAsyncOperation);
+            await Task.WhenAll(trueTasks);
+        }
+
+        public static async Task SingleTrueAsyncOperation(int taskNumber)
+        {
+            Console.WriteLine($"Doing task with number: {taskNumber}");
+            await Task.Delay(5000);
+            Console.WriteLine($"Finished task with number: {taskNumber}");
+        }
+
+        public static async Task SingleFakeAsyncOperation(int taskNumber)
+        {
+            Console.WriteLine($"Doing task with number: {taskNumber}");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Finished task with number: {taskNumber}");
+        }
+    }
+}
